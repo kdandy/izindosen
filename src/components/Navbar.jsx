13e96b8dfb1c108978e8180f95af89073a4f865d
dandy <50922642+kdandy@users.noreply.github.com>
2023-09-08 16:41:28 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { AiFillGithub } from "react-icons/ai";
 const Navbar = () => {
   return (
     <nav className="w-full flex justify-between items-center border-b border-slaye-400 py-4 px-3 lg:px-20">
-      <h1 className="font-bold italic">SopanDikit🙏🏻</h1>
+      <h1 className="font-bold italic">Minimal Izin🙏🏻</h1>
       <a
-        href="https://github.com/Taufik-H/sopandikit/tree/main"
+        href="https://github.com/kdandy/izindosen/tree/main"
         className="border rounded-md p-2 flex gap-2 items-center hover:bg-slate-100 transition-all duration-200 "
       >
         <AiFillGithub size={20} />
-        Taufik-H/sopandikit
+        kdandy/izindosen
       </a>
     </nav>
   );
